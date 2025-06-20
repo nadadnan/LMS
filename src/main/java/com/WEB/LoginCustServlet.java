0947c130c5a58fdb824cdcfa940895d2f6cd8832
M S I <M S I@172.17.163.212>
2025-06-20 09:08:36 +0800

    package Commit

--- src/main/java/com/WEB/LoginCustServlet.java
+++ src/main/java/com/WEB/LoginCustServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import java.com.DAO.DBUtil;
+import com.DAO.DBUtil;
 /**
  *
  * Author: NUR NADIYATUL HUSNA BINTI ADNAN (S65470)
